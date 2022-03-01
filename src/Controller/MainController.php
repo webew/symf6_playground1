1aa84a5e80b2e61d3e548f3a11bc0cab7a0e6daa
webew <fillion.bertrand@gmail.com>
2022-03-01 13:12:37 +0100

    intro Doctrine

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Categorie;
+use App\Repository\CategorieRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -21,5 +24,34 @@ class MainController extends AbstractController
     {
         return $this->render('main/about.html.twig');
     }
-    
+
+    #[Route('/test', name: 'app_test')]
+    public function test(ManagerRegistry $doctrine): Response
+    {
+        $entityManager = $doctrine->getManager();
+
+        $cat = new Categorie();
+        $cat->setNom("Nouvelles technologies");
+        $cat->setDescription("La catégorie consacrée aux nouvelles technologies.");
+
+        $entityManager->persist($cat);
+
+        $entityManager->flush();
+        
+
+        return $this->render('main/test.html.twig');
+    }
+
+    #[Route('/voirCategories', name: 'app_voir_categories')]
+    public function voirCategories(CategorieRepository $categorieRepository): Response
+    {
+        $categories = $categorieRepository->findAll(); // $categories contient un array d'objets de type Categorie
+        // dd($categories);
+
+        foreach($categories as $categorie){
+            dump($categorie->getNom());
+        }
+
+        return $this->render('main/voirCategories.html.twig', ["categories" => $categories]);
+    }
 }
---
+++ src/Entity/Categorie.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CategorieRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CategorieRepository::class)]
+class Categorie
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $nom;
+
+    #[ORM\Column(type: 'text', nullable: true)]
+    private $description;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+}
